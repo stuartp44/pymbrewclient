0460d78828b2686377032412b5a3c449a15b580f
Stuart Pearson <1926002+stuartp44@users.noreply.github.com>
2025-04-27 23:48:15 +0200

    feat: Update __all__ in __init__.py to include new models for better API exposure

--- src/pymbrewclient/__init__.py
+++ src/pymbrewclient/__init__.py
@@ -35,7 +35,15 @@
 #
 # Disclaimer: This software is an independent project and is not affiliated with, endorsed by, or associated with MiniBrew. MiniBrew's trademarks, logos, API, and other intellectual property are owned by MiniBrew and are not included in this software. Users are responsible for complying with MiniBrew's terms of service when using this software.
 from .client import BreweryClient
+from .rest.models import Device, TokenResponse, ApiResponse, BreweryOverview, Session, DeviceDetails, Beer
 
 __all__ = [
     "BreweryClient",
+    "Device",
+    "TokenResponse",
+    "ApiResponse",
+    "BreweryOverview",
+    "Session",
+    "DeviceDetails",
+    "Beer"
 ]
