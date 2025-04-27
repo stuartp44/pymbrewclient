f135508728cb80011fbbaf4905c7ab95a38c0f1a
Stuart Pearson <1926002+stuartp44@users.noreply.github.com>
2025-04-27 22:19:03 +0200

    fix: Rename class pymbrewclient to breweryclient for clarity

--- src/pymbrewclient/pymbrewclient.py
+++ src/pymbrewclient/pymbrewclient.py
@@ -38,7 +38,7 @@ from pymbrewclient.rest.client import RestApiClient
 from pymbrewclient.rest.models import TokenResponse, BreweryOverview, Session
 
 
-class pymbrewclient:
+class breweryclient:
     """
     A client for interacting with the Minibrew Pro Portal API.
     """
